c9868f289dc6c48a6ebc2e1d0d0d48a0663ee4b0
sev-valdez <severianovaldez@lithia.com>
2023-10-28 22:18:34 -0400

    Add express examples

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+const PORT = 3000;
+
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    // Actions go here...
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
+});
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My Friends Are Very Clever',
+        caption: 'Cute little puppy!',
+    });
+});
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`);
+});
